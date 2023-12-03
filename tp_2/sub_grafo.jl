3d40e4c5f48250429477ea28ba06ac32cd2276f1
marcelomrad <marcelomrad@gmail.com>
2023-12-03 12:11:21 -0300

    feat: sub grafo feito

--- tp_2/sub_grafo.jl
+++ tp_2/sub_grafo.jl
@@ -1,103 +1,77 @@
+# Função para abrir o arquivo
+function abrir_arquivo(nome_arquivo)
+    open(nome_arquivo, "r")
+end
+
 # Função para ler os dados do arquivo
 function ler_dados_subgrafo(nome_arquivo)
-    arquivo = open(nome_arquivo, "r")
-    n = parse(Int, split(readline(arquivo), '\t')[2])
-    arestas_pesos = Tuple{Int, Int, Float64}[]
-    for linha in eachline(arquivo)
-        tokens = split(linha, '\t')
-        push!(arestas_pesos, (parse(Int, tokens[2]), parse(Int, tokens[3]), parse(Float64, tokens[4])))
+    arquivo = abrir_arquivo(nome_arquivo)
+    num_vertices = parse(Int, split(readline(arquivo), '\t')[2])
+    matriz_arestas = zeros(Int8, (num_vertices, num_vertices))
+    matriz_pesos = zeros(Int16, (num_vertices, num_vertices))
+    arestas_pesos = []
+
+    while !eof(arquivo)
+        s = split(readline(arquivo), '\t')
+        u, v, w = parse(Int, s[2]), parse(Int, s[3]), parse(Int16, s[4])
+        matriz_pesos[u, v], matriz_pesos[v, u] = w, w
+        matriz_arestas[u, v], matriz_arestas[v, u] = 1, 1
+        push!(arestas_pesos, (u, v, w))
     end
     close(arquivo)
-    return n, arestas_pesos
+    num_vertices, matriz_arestas, matriz_pesos, arestas_pesos
 end
 
-# Função para calcular o peso do subgrafo
-function calcular_peso(solucao, arestas_pesos)
-    peso = 0.0
-    for (v, u, w) in arestas_pesos
-        if v in solucao && u in solucao
-            peso += w
+# Função para atualizar as heurísticas
+function atualizar_heuristicas(heuristica_vertices, matriz_arestas, matriz_pesos, solucao, min_indx)
+    for i in eachindex(heuristica_vertices)
+        if matriz_arestas[min_indx, i] == 1 && solucao[i] == 1
+            heuristica_vertices[i] -= matriz_pesos[min_indx, i]
         end
     end
-    return peso
+    heuristica_vertices[min_indx] = 0 
+    solucao[min_indx] = 0
+    heuristica_vertices
 end
 
-using Random
 
-# Função para criar uma solução aleatória
-function criar_solucao_aleatoria(n)
-    solucao = rand(1:n, rand(1:n))
-    return Set(solucao)
-end
+# Função para resolver o problema do maior subgrafo induzido
+function resolver_subgrafo(nome_arquivo)
+    num_vertices, matriz_arestas, matriz_pesos, arestas_pesos = ler_dados_subgrafo(nome_arquivo)
 
-# Função para criar a população inicial
-function criar_populacao_inicial(tamanho_populacao, n)
-    return [criar_solucao_aleatoria(n) for _ in 1:tamanho_populacao]
-end
-
-# Função de seleção - torneio, sem usar 'sample'
-function selecionar_para_reproducao(populacao, arestas_pesos, tamanho_torneio)
-    selecionados = []
-    for _ in 1:length(populacao)
-        candidatos = [populacao[rand(1:end)] for _ in 1:tamanho_torneio]
-        melhor = findmax([calcular_peso(s, arestas_pesos) for s in candidatos])[2]
-        push!(selecionados, candidatos[melhor])
+    heuristica_vertices = zeros(Int16, num_vertices)
+    for (u, v, w) in arestas_pesos
+        heuristica_vertices[u] += w
+        heuristica_vertices[v] += w
     end
-    return selecionados
-end
 
+    solucao = ones(Int8, num_vertices)
+    min_indx = argmin(heuristica_vertices)
 
-# Função de crossover - ponto único, sem usar 'take'
-function crossover(solucao1, solucao2)
-    solucao1_list = collect(solucao1)
-    solucao2_list = collect(solucao2)
-
-    ponto = rand(1:min(length(solucao1_list), length(solucao2_list)))
-    filho1 = Set(solucao1_list[1:ponto])
-    filho2 = Set(solucao2_list[1:ponto])
-
-    filho1 = union(filho1, Set(solucao2_list[ponto+1:end]))
-    filho2 = union(filho2, Set(solucao1_list[ponto+1:end]))
-
-    return filho1, filho2
-end
-
-# Função de mutação - adicionar ou remover um vértice
-function mutacao(solucao, n)
-    if rand() < 0.5
-        push!(solucao, rand(1:n))
-    else
-        solucao = setdiff(solucao, Set([rand(collect(solucao))]))
+    while heuristica_vertices[min_indx] < 0
+        heuristica_vertices = atualizar_heuristicas(heuristica_vertices, matriz_arestas, matriz_pesos, solucao, min_indx)
+        min_indx = argmin(heuristica_vertices)
     end
-    return solucao
+
+    calcular_total(arestas_pesos, solucao)
 end
 
-# Algoritmo genético principal
-function resolver_subgrafo(nome_arquivo, tamanho_populacao, geracoes, tamanho_torneio)
-    n, arestas_pesos = ler_dados_subgrafo(nome_arquivo)
-    populacao = criar_populacao_inicial(tamanho_populacao, n)
+# Função para calcular e imprimir o total
+function calcular_total(arestas_pesos, solucao)
+    total = sum(w for (u, v, w) in arestas_pesos if solucao[u] == 1 && solucao[v] == 1)
 
-    for _ in 1:geracoes
-        populacao = selecionar_para_reproducao(populacao, arestas_pesos, tamanho_torneio)
-        nova_populacao = []
-        for i in 1:2:length(populacao)-1
-            filho1, filho2 = crossover(populacao[i], populacao[i+1])
-            push!(nova_populacao, mutacao(filho1, n))
-            push!(nova_populacao, mutacao(filho2, n))
+    println("TP_2 2021031548 = ", total)
+    println("VÉRTICES:")
+    for (i, val) in enumerate(solucao)
+        if val == 1
+            println(i)
         end
-        populacao = nova_populacao
-    end
-
-    melhor_solucao = findmax([calcular_peso(s, arestas_pesos) for s in populacao])[2]
-    println("VALOR = ", calcular_peso(populacao[melhor_solucao], arestas_pesos))
-    println("VERTICES:")
-    for vertice in sort(collect(populacao[melhor_solucao]))
-        println(vertice)
     end
 end
 
+# Execução do programa
 if length(ARGS) > 0
-    resolver_subgrafo(ARGS[1], 100, 120 ,7)
+    resolver_subgrafo(ARGS[1])
 else
     println("Por favor, forneça o nome do arquivo como argumento.")
 end
